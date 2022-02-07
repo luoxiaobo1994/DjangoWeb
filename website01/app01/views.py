92bcaea0e36e05a003a3e53d05872c58765864ae
luoxiaobo <438119195@qq.com>
2022-02-07 23:30:56 +0800

    增加了一个重定向功能

--- website01/app01/views.py
+++ website01/app01/views.py
@@ -39,11 +39,10 @@ def something(request):
 def login(request):
     if request.method == "GET":
         return render(request, "login.html")
-    else:
-        # 如果是post请求,则获取用户提交的数据
-        username = request.POST.get("user")
-        password = request.POST.get("pwd")
-        if username == 'root' and password == '123':
-            return HttpResponse("登录成功")
-        else:
-            return HttpResponse("登录失败")
+    # 如果是post请求,则获取用户提交的数据
+    username = request.POST.get("user")
+    password = request.POST.get("pwd")
+    if username == 'root' and password == '123':
+        # return HttpResponse("登录成功")
+        return redirect("https://www.baidu.com")
+    return render(request, "login.html", {"error_msg": "用户名或密码错误"})
